c50387614d5901711279e603bdd44e02e48b126b
Howard Yeh <howard@metacircus.com>
2016-08-09 17:38:57 +0800

    reverse numbers

--- forEach/reverse-array.js
+++ forEach/reverse-array.js
@@ -5,7 +5,7 @@
 
 输出：
 
-[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+[ 6, 5, 4, 3, 2, 1 ]
 */
 
 var numbers = [
@@ -16,4 +16,4 @@ var reversedNumbers = [];
 
 // ???
 
-console.log(allNumbers);
+console.log(reversedNumbers);
