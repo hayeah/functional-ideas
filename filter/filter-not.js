038edd499ebf2fe63a803f2159373e63cd8897d6
Howard Yeh <howard@metacircus.com>
2016-08-11 10:51:28 +0800

    filter-not exercise

---
+++ filter/filter-not.js
@@ -0,0 +1,22 @@
+/*
+用 filter 实现 filterNot。 filterNot 保留不符合条件的元素。
+*/
+
+
+const { equal } = require("../equal");
+
+function filterNot(array, test) {
+	return array.filter(o => !test(o));
+}
+
+equal(
+	filterNot([1, 2, 3, 2, 1], x => x === 1),
+	[2, 3, 2]
+);
+
+equal(
+	filterNot([1, -1, 2, -2, 3, -3], x => x > 0),
+	[-1, -2, -3]
+);
+
+
